4765cee1c244d4302217ca73b170e6c65d8c79c6
HninYuKhaing <51561320+HninYuKhaing97@users.noreply.github.com>
2021-10-10 06:00:08 -0800

    Prohibited App Check Tool with C# Windows Form

---
+++ ProhibitedAppCheckTool_Form.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Prohibited_App_Check_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMachineName = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.rdEng = new System.Windows.Forms.RadioButton();
+            this.rdJp = new System.Windows.Forms.RadioButton();
+            this.lblApp = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtApp = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnInputFileSelect = new System.Windows.Forms.Button();
+            this.btnOutputSelect = new System.Windows.Forms.Button();
+            this.rdPartial = new System.Windows.Forms.RadioButton();
+            this.rdBeginning = new System.Windows.Forms.RadioButton();
+            this.rdPerfect = new System.Windows.Forms.RadioButton();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblMachineName
+            // 
+            this.lblMachineName.AutoSize = true;
+            this.lblMachineName.Location = new System.Drawing.Point(35, 27);
+            this.lblMachineName.Name = "lblMachineName";
+            this.lblMachineName.Size = new System.Drawing.Size(104, 15);
+            this.lblMachineName.TabIndex = 0;
+            this.lblMachineName.Text = "Machine Name:";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(399, 27);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(41, 15);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "Time:";
+            // 
+            // rdEng
+            // 
+            this.rdEng.AutoSize = true;
+            this.rdEng.Checked = true;
+            this.rdEng.Location = new System.Drawing.Point(553, 55);
+            this.rdEng.Name = "rdEng";
+            this.rdEng.Size = new System.Drawing.Size(46, 19);
+            this.rdEng.TabIndex = 2;
+            this.rdEng.TabStop = true;
+            this.rdEng.Text = "EN";
+            this.rdEng.UseVisualStyleBackColor = true;
+            this.rdEng.CheckedChanged += new System.EventHandler(this.rdEng_CheckedChanged);
+            // 
+            // rdJp
+            // 
+            this.rdJp.AutoSize = true;
+            this.rdJp.Location = new System.Drawing.Point(605, 55);
+            this.rdJp.Name = "rdJp";
+            this.rdJp.Size = new System.Drawing.Size(45, 19);
+            this.rdJp.TabIndex = 3;
+            this.rdJp.Text = "JP";
+            this.rdJp.UseVisualStyleBackColor = true;
+            this.rdJp.CheckedChanged += new System.EventHandler(this.rdJp_CheckedChanged);
+            // 
+            // lblApp
+            // 
+            this.lblApp.AutoSize = true;
+            this.lblApp.Location = new System.Drawing.Point(35, 102);
+            this.lblApp.Name = "lblApp";
+            this.lblApp.Size = new System.Drawing.Size(101, 15);
+            this.lblApp.TabIndex = 4;
+            this.lblApp.Text = "Prohibited App:";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(35, 164);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(131, 15);
+            this.lblOutput.TabIndex = 5;
+            this.lblOutput.Text = "Output Destination:";
+            // 
+            // txtApp
+            // 
+            this.txtApp.Location = new System.Drawing.Point(180, 99);
+            this.txtApp.Name = "txtApp";
+            this.txtApp.Size = new System.Drawing.Size(354, 22);
+            this.txtApp.TabIndex = 6;
+            this.txtApp.TextChanged += new System.EventHandler(this.txtApp_TextChanged);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(180, 159);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(354, 22);
+            this.txtOutput.TabIndex = 7;
+            this.txtOutput.TextChanged += new System.EventHandler(this.txtOutput_TextChanged);
+            // 
+            // btnInputFileSelect
+            // 
+            this.btnInputFileSelect.Location = new System.Drawing.Point(554, 98);
+            this.btnInputFileSelect.Name = "btnInputFileSelect";
+            this.btnInputFileSelect.Size = new System.Drawing.Size(95, 23);
+            this.btnInputFileSelect.TabIndex = 8;
+            this.btnInputFileSelect.Text = "Select";
+            this.btnInputFileSelect.UseVisualStyleBackColor = true;
+            this.btnInputFileSelect.Click += new System.EventHandler(this.btnInputFileSelect_Click);
+            // 
+            // btnOutputSelect
+            // 
+            this.btnOutputSelect.Location = new System.Drawing.Point(556, 160);
+            this.btnOutputSelect.Name = "btnOutputSelect";
+            this.btnOutputSelect.Size = new System.Drawing.Size(95, 23);
+            this.btnOutputSelect.TabIndex = 9;
+            this.btnOutputSelect.Text = "Select";
+            this.btnOutputSelect.UseVisualStyleBackColor = true;
+            this.btnOutputSelect.Click += new System.EventHandler(this.btnOutputSelect_Click);
+            // 
+            // rdPartial
+            // 
+            this.rdPartial.AutoSize = true;
+            this.rdPartial.Location = new System.Drawing.Point(3, 16);
+            this.rdPartial.Name = "rdPartial";
+            this.rdPartial.Size = new System.Drawing.Size(248, 19);
+            this.rdPartial.TabIndex = 10;
+            this.rdPartial.Text = "Partial Match      (xxxappnamexxx)";
+            this.rdPartial.UseVisualStyleBackColor = true;
+            this.rdPartial.CheckedChanged += new System.EventHandler(this.rdPartial_CheckedChanged);
+            // 
+            // rdBeginning
+            // 
+            this.rdBeginning.AutoSize = true;
+            this.rdBeginning.Checked = true;
+            this.rdBeginning.Location = new System.Drawing.Point(3, 64);
+            this.rdBeginning.Name = "rdBeginning";
+            this.rdBeginning.Size = new System.Drawing.Size(225, 19);
+            this.rdBeginning.TabIndex = 11;
+            this.rdBeginning.TabStop = true;
+            this.rdBeginning.Text = "Beginning Match (appnamexxx)";
+            this.rdBeginning.UseVisualStyleBackColor = true;
+            this.rdBeginning.CheckedChanged += new System.EventHandler(this.rdBeginning_CheckedChanged);
+            // 
+            // rdPerfect
+            // 
+            this.rdPerfect.AutoSize = true;
+            this.rdPerfect.Location = new System.Drawing.Point(3, 115);
+            this.rdPerfect.Name = "rdPerfect";
+            this.rdPerfect.Size = new System.Drawing.Size(205, 19);
+            this.rdPerfect.TabIndex = 12;
+            this.rdPerfect.Text = "Perfect Match    (appname)\r\n";
+            this.rdPerfect.UseVisualStyleBackColor = true;
+            this.rdPerfect.CheckedChanged += new System.EventHandler(this.rdPerfect_CheckedChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(38, 371);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(109, 36);
+            this.btnStart.TabIndex = 13;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rdPerfect);
+            this.panel1.Controls.Add(this.rdBeginning);
+            this.panel1.Controls.Add(this.rdPartial);
+            this.panel1.Location = new System.Drawing.Point(38, 201);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(269, 158);
+            this.panel1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 457);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnOutputSelect);
+            this.Controls.Add(this.btnInputFileSelect);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtApp);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblApp);
+            this.Controls.Add(this.rdJp);
+            this.Controls.Add(this.rdEng);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblMachineName);
+            this.Name = "Form1";
+            this.Text = "Prohibited App Check Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMachineName;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.RadioButton rdEng;
+        private System.Windows.Forms.RadioButton rdJp;
+        private System.Windows.Forms.Label lblApp;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtApp;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnInputFileSelect;
+        private System.Windows.Forms.Button btnOutputSelect;
+        private System.Windows.Forms.RadioButton rdPartial;
+        private System.Windows.Forms.RadioButton rdBeginning;
+        private System.Windows.Forms.RadioButton rdPerfect;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ProhibitedAppCheckTool_Form.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prohibited_App_Check_Tool
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            //端末名と実施日
+            lblMachineName.Text = "MachineName:" + Environment.MachineName;
+            lblTime.Text = "Time:" + DateTime.Now.ToString();
+        }
+
+
+
+        private void rdEng_CheckedChanged(object sender, EventArgs e)
+        {
+            //ラジオENを押す場合、言語は英語に変更する
+            if (rdEng.Checked == true)
+            {
+                lblMachineName.Text = "MachineName:" + Environment.MachineName;
+                lblTime.Text = "Time:" + DateTime.Now.ToString();
+                lblApp.Text = "Prohibited App:";
+                lblOutput.Text = "Output Destination:";
+                btnInputFileSelect.Text = "Select";
+                btnOutputSelect.Text = "Select";
+                rdPartial.Text = "Partial Match       (xxxappnamexxx)";
+                rdBeginning.Text = "Beginning Match (appnamexxx)";
+                rdPerfect.Text = "Perfect Match     (appname)";
+                btnStart.Text = "Start";
+            }
+        }
+
+        private void rdJp_CheckedChanged(object sender, EventArgs e)
+        {
+            //ラジオJPを押す場合、言語は日本語に変更する
+            if (rdJp.Checked == true)
+            {
+                lblMachineName.Text = "端末目:" + Environment.MachineName;
+                lblTime.Text = "実施日:" + DateTime.Now.ToString();
+                lblApp.Text = "使用禁止アプリ:";
+                lblOutput.Text = "出力先:";
+                btnInputFileSelect.Text = "参照";
+                btnOutputSelect.Text = "参照";
+                rdPartial.Text = "部分一致      (xxxappnamexxx)";
+                rdBeginning.Text = "先頭一致     (appnamexxx)";
+                rdPerfect.Text = "完全一致     (appname)";
+                btnStart.Text = "スタート";
+            }
+        }
+
+        private void txtApp_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInputFileSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            DialogResult result = fileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtApp.Text = fileDialog.FileName;
+            }
+            else
+            {
+                MessageBox.Show("Please choose prohibited app input file!");
+            }
+        }
+
+        private void btnOutputSelect_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtOutput.Text = folderDlg.SelectedPath;
+            }
+            else
+            {
+                MessageBox.Show("Please choose output destination!");
+            }
+        }
+
+        private void rdPartial_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdBeginning_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdPerfect_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //パソコンの中のドライブ全部を取得
+            String[] strDirectoryArr = new string[24];
+            Int32 intarrIndex;
+            Int32 intIndex;
+            intarrIndex = 0;
+            String[] strExeFileArr = new string[500];
+            String[] TempArr = new string[500];
+            int intfileIndex;
+            intfileIndex = 0;
+            bool blnHasFolder = false;
+            String[] strInputfileArr = new string[300];
+
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in allDrives)
+            {
+                if (Directory.Exists(drive.Name))
+                {
+                    strDirectoryArr[intarrIndex] = drive.Name;
+                    intarrIndex = intarrIndex + 1;
+                }
+                else
+                {
+                    //Nothing
+                }
+            }
+
+            //ドライブの中のファイルを取得            
+            for (intIndex = 0; intIndex < strDirectoryArr.Length; intIndex++)
+            {
+                if (strDirectoryArr[intIndex] != null)
+                {
+                    String strPath = @"d:\Drives";
+                    //String strPath = strDirectoryArr[intIndex];
+                    //MessageBox.Show(strDirectoryArr[intIndex]);
+                    //ReadSoftware(@strDirectoryArr[intIndex]);
+                    //Get file
+                    string[] fileEntries = Directory.GetFiles(strPath);
+                    foreach (string fileName in fileEntries)
+                    {
+                        string filepath = Path.GetFileName(fileName);
+                        string strExtension = Path.GetExtension(filepath);
+                        if (strExtension == ".exe")
+                        {
+                            strExeFileArr[intfileIndex] = filepath;
+                            //MessageBox.Show(strExeFileArr[intfileIndex]);
+                            intfileIndex = intfileIndex + 1;
+                        }
+                    }
+                    //MessageBox.Show(string.Join(",", strExeFileArr));
+
+                    //GetFolder                    
+                    string[] subdirectoryEntries = Directory.GetDirectories(strPath);
+                    if (subdirectoryEntries.Length != 0)
+                    {
+                        blnHasFolder = true;
+                    }
+                    else
+                    {
+                        blnHasFolder = false;
+                    }
+
+                    while (blnHasFolder == true)
+                    {
+                        foreach (string subdirectory in subdirectoryEntries)
+                        {
+                            if (subdirectory != null)
+                            {
+                                //GetFile in subfolder
+                                try
+                                {
+                                    string[] file = Directory.GetFiles(subdirectory);
+                                    foreach (string fileName in file)
+                                    {
+                                        string filepath = Path.GetFileName(fileName);
+                                        string strExtension = Path.GetExtension(filepath);
+                                        if (strExtension == ".exe")
+                                        {
+                                            strExeFileArr[intfileIndex] = filepath;
+                                            //strPath[intfileIndex] = subdirectory +"\\"+ filepath;
+                                            //MessageBox.Show(strExeFileArr[intfileIndex]);
+                                            intfileIndex = intfileIndex + 1;
+                                            //blnHasFolder = true;
+                                        }
+
+                                    }//foreach filename
+                                }//try
+                                catch
+                                {
+                                    //error message
+                                }//catch
+                            }
+                            else
+                            {
+                                //If null,do Nothing
+                            }
+
+                        }//foreach subdirectory
+                        //MessageBox.Show(string.Join(",", strExeFileArr));
+
+                        //Find subfolder from subfolder
+                        int folderindex = 0;
+                        foreach (string subdirectory in subdirectoryEntries)
+                        {
+                            if (subdirectory != null)
+                            {
+                                try
+                                {
+                                    string[] subfolderArr = Directory.GetDirectories(subdirectory);
+                                    blnHasFolder = true;
+                                    foreach (string subfolder in subfolderArr)
+                                    {
+                                        TempArr[folderindex] = subfolder;
+                                        folderindex = folderindex + 1;
+                                    }
+                                }
+                                catch
+                                {
+                                    //errorMessage
+                                }
+                            }
+                            else
+                            {
+                                //If null,do Nothing
+                            }
+                        }//foreach subdirectory
+                        //MessageBox.Show(string.Join(",", strExeFileArr));
+
+                        if (folderindex == 0)
+                        {
+                            blnHasFolder = false;
+                        }
+                        else
+                        {
+                            blnHasFolder = true;
+                            int TempArrLength = TempArr.Length;
+                            //Array.Clear(subdirectoryEntries, 0, subdirectoryEntries.Length);
+                            subdirectoryEntries = new string[500];
+                            //subdirectoryEntries = (string[])TempArr.Clone();
+                            //TempArr.CopyTo(subdirectoryEntries, TempArrLength);
+                            Array.Clear(subdirectoryEntries, 0, 500);
+                            //MessageBox.Show(string.Join(",", subdirectoryEntries));
+                            Array.Copy(TempArr, subdirectoryEntries, TempArrLength);
+                            //MessageBox.Show(string.Join(",", subdirectoryEntries));
+                        }
+                    }//while
+                    MessageBox.Show(string.Join(",", strExeFileArr));
+                    //MessageBox.Show("Hello");
+                }
+                else
+                {
+                    //Nothing
+                }
+            }
+            //MessageBox.Show(string.Join(",", strExeFileArr));
+
+            //Read Input file
+            ReadInputFile(strInputfileArr);
+            if (rdPartial.Checked == true)
+            {
+                PartialMatch(strExeFileArr, strInputfileArr);
+            }
+            else if (rdBeginning.Checked == true)
+            {
+                BeginningMatch(strExeFileArr, strInputfileArr);
+            }
+            else if (rdPerfect.Checked == true)
+            {
+                PerfectMatch(strExeFileArr, strInputfileArr);
+            }
+            else
+            {
+                //Nothing
+            }
+        }
+
+        public void ReadInputFile(String[] strLineArr)
+        {
+            MessageBox.Show("start");
+            string strInputFilePath;
+            string line;
+            //String[] strLineArr = new string[500];
+            int lineIndex = 0;
+            //Get the Input file path
+            strInputFilePath = txtApp.Text;
+            System.IO.StreamReader file = new System.IO.StreamReader(@strInputFilePath);
+            while ((line = file.ReadLine()) != null)
+            {
+                strLineArr[lineIndex] = line;
+                lineIndex = lineIndex + 1;
+            }
+            file.Close();
+        }
+
+        //分部一致
+        public void PartialMatch(String[] strExeFileArr, String[] strInputfileArr)
+        {
+            foreach (string strInputName in strInputfileArr)
+            {
+                strInputName.Replace(" ", "_");
+                strInputName.Replace("-", "_");
+                string strDotPosition = strInputName.Replace(".", string.Empty);
+
+                if (strDotPosition != null)
+                {
+                    int intDotPosition = Int32.Parse(strDotPosition);
+                    strInputName.Substring(0, intDotPosition - 1);
+                    strInputName.ToLower();
+                }
+            }
+            for (int softwareName = 0; softwareName <= strExeFileArr.Length; softwareName++)
+            {
+                string strSoftwareName = softwareName.ToString();
+                strSoftwareName.Replace(" ", "_");
+                strSoftwareName.Replace("-", "_");
+                string strDotPosition = strSoftwareName.Replace(".", string.Empty);
+                if (strDotPosition != null)
+                {
+                    int intDotPosition = Int32.Parse(strDotPosition);
+                    strSoftwareName.Substring(0, intDotPosition - 1);
+                    strSoftwareName.ToLower();
+                }
+            }
+        }
+
+        //先頭一致
+        public void BeginningMatch(String[] strExeFileArr, String[] strInputfileArr)
+        {
+
+        }
+
+        //完全一致
+        public void PerfectMatch(String[] strExeFileArr, String[] strInputfileArr)
+        {
+
+        }
+        public int intfileIndex = 0;
+        /*public static void ReadSoftware(string workingDirectory)
+        {
+            String[] strExeFileArr = new string[500];
+            int intfileIndex;
+            
+            try
+            {
+                foreach (string d in Directory.GetDirectories(workingDirectory))
+                {
+                    ReadSoftware(d);
+                }
+
+                foreach (string f in Directory.GetFiles(workingDirectory))
+                {
+                    string filepath = Path.GetFileName(f);
+                    string strExtension = Path.GetExtension(filepath);
+                    if (strExtension == ".exe")
+                    {
+                        strExeFileArr[intfileIndex] = filepath;
+                        intfileIndex = intfileIndex + 1;
+                    }
+                }
+
+            }
+            catch (System.Exception)
+            {
+                //MessageBox.Show(ex.Message);  
+            }
+            MessageBox.Show("Finish");
+        }
+        */
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    [Serializable]
+    internal class AnotherTypeOfException : Exception
+    {
+        public AnotherTypeOfException()
+        {
+        }
+
+        public AnotherTypeOfException(string message) : base(message)
+        {
+        }
+
+        public AnotherTypeOfException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AnotherTypeOfException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
